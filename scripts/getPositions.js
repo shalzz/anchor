3712af7c91b7a9108d2ce9f5f2786657aac10521
Nour Haridy <nourharidy@gmail.com>
2021-03-11 10:35:00 +0200

    add getPositions script

---
+++ scripts/getPositions.js
@@ -0,0 +1,33 @@
+const hre = require('hardhat');
+const {deployments, ethers} = hre;
+const Table = require('cli-table');
+
+(async () => {
+    const comptrollerDeployment = await deployments.get("Comptroller")
+    const comptroller = new ethers.Contract(comptrollerDeployment.address, comptrollerDeployment.abi, ethers.provider)
+    const filter = comptroller.filters.MarketEntered();
+    const logs = await comptroller.queryFilter(filter, 0, "latest");
+    let accounts = [...new Set(logs.map(v=>v.args.account))]
+    const accountsLiquidity = await Promise.all(accounts.map(v => comptroller.getAccountLiquidity(v)))
+    var positionTable = new Table({
+        head: ['Address', 'Liquidity', 'Shortfall', 'Liquidatable']
+    });
+    let positions = accounts.map((v,i) => [v, accountsLiquidity[i][1], accountsLiquidity[i][2], accountsLiquidity[i][2].gt(0)])
+    positions = positions.filter((v) => v[1].gt(0) || v[2].gt(0))
+    positions = positions.map(v => [v[0], ethers.utils.formatEther(v[1]), ethers.utils.formatEther(v[2]), v[3]])
+    positionTable.push(...positions)
+
+    console.log("Positions:")
+    console.log(positionTable.toString());
+
+    var liquidatableTable = new Table({
+        head: ['Address', 'Liquidity', 'Shortfall', 'Liquidatable']
+    });
+
+    const liquidatable = positions.filter(v => v[3] === true)
+    liquidatableTable.push(...liquidatable)
+
+    console.log("Liquidatable:")
+    console.log(liquidatableTable.toString());
+
+})();
