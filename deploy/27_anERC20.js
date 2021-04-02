e18950260ab6eb2023069ed8feb38aaaf83653c7
Nour Haridy <nourharidy@gmail.com>
2021-04-02 23:26:46 +0200

    add market deploy script

---
+++ deploy/27_anERC20.js
@@ -0,0 +1,36 @@
+var prompt = require('prompt-sync')();
+
+module.exports = async ({
+    deployments,
+    getNamedAccounts
+  }) => {
+    console.log("27. Deploy anERC20 market")
+    const {deploy, save} = deployments;
+
+    const Unitroller = await deployments.get('Unitroller');
+    const Model = await deployments.get('DolaInterestRateModel');
+    const {deployer} = await getNamedAccounts();
+
+    const name = prompt('Market name: ');
+    const symbol = prompt('Market symbol: ');
+    const underlyingAddress = prompt('Underlying token address: ');
+
+    await deploy('CErc20Immutable', {
+      from: deployer,
+      args:[
+        underlyingAddress,
+        Unitroller.address,
+        Model.address,
+        "200000000000000000000000000",
+        name,
+        symbol,
+        "8",
+        deployer
+      ]
+    });
+    const market = await deployments.get('CErc20Immutable');
+    await save(name, market);
+  };
+
+module.exports.dependencies = ['DolaInterestRateModel', 'Unitroller'];
+module.exports.tags = ['anERC20'];
