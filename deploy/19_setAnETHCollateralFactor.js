3d35a22973bcf745e0750f4d9d3d38517f0bb86a
Masaud <sophismay@gmail.com>
2021-04-25 23:45:03 +0200

    fix: removing hardhat added local config and checks
    hardhat config: removed local network indices from named accounts
    deploy files: removed local network checks from deployment files
    added deployment/* files that were removed from git

--- deploy/19_setAnETHCollateralFactor.js
+++ deploy/19_setAnETHCollateralFactor.js
@@ -1,50 +1,20 @@
-const { network } = require("hardhat");
-
 module.exports = async ({
-    deployments,
-    getNamedAccounts,
-  }) => {
-    console.log("19. Set AnETH Collateral Factor to 50%")
-    const { deploy, execute, save } = deployments;
-    const {deployer} = await getNamedAccounts()
+  deployments,
+  getNamedAccounts,
+}) => {
+  console.log("19. Set AnETH Collateral Factor to 50%")
+  const {execute} = deployments;
+  const {deployer} = await getNamedAccounts()
 
-    // deploy mock oracle feed if network is local/hardhat
-    // Comptroller._setCollateralFactor calls Oracle which calls feed when computing underlying price
-    const network_ = network.name;
-    if (network_ == 'hardhat' || network_ == 'localhost') {
-      await deploy(
-        'OracleFeed',
-        {
-          from: deployer,
-          args: [
-            18 // anETH
-          ]
-        }
-      );
-      const OracleFeed = await deployments.get('OracleFeed');
-      await save('OracleFeed', OracleFeed);
-
-      // now set feed for Oracle
-      // get address of deployed OracleFeed and set that as feed for Oracle to use
-      await execute('Oracle', {
-          from: deployer,
-        },
-          "setFeed",
-          (await deployments.get('anETH')).address,
-          OracleFeed.address,
-          18
-      )
-    }
-
-    await execute('Comptroller', {
-        from: deployer
-    },
-        "_setCollateralFactor",
-        (await deployments.get('anETH')).address,
-        "500000000000000000" // 50%
-    )
-    return true
-  };
+  await execute('Comptroller', {
+      from: deployer
+  },
+      "_setCollateralFactor",
+      (await deployments.get('anETH')).address,
+      "500000000000000000" // 50%
+  )
+  return true
+};
 module.exports.id = 'collateralFactor'
 module.exports.tags = ['collateralFactor'];
 module.exports.dependencies = ['addEthMarket'];
--- deploy/26_anYFI.js
+++ deploy/26_anYFI.js
@@ -1,68 +1,32 @@
-const { network } = require("hardhat");
 
 module.exports = async ({
-    deployments,
-    getNamedAccounts
-  }) => {
-    console.log("26. Deploy anYFI")
-    const {deploy, save, execute} = deployments;
+  deployments,
+  getNamedAccounts
+}) => {
+  console.log("26. Deploy anYFI")
+  const {deploy, save} = deployments;
 
-    const Unitroller = await deployments.get('Unitroller');
-    const Model = await deployments.get('DolaInterestRateModel');
-    let {deployer, yfi, delegateRegistry} = await getNamedAccounts();
+  const Unitroller = await deployments.get('Unitroller');
+  const Model = await deployments.get('DolaInterestRateModel');
+  const {deployer, yfi, delegateRegistry} = await getNamedAccounts();
 
-    // if network is hardhat, create and mint some underlying (yfi) tokens
-    const network_ = network.name;
-    if (network_ == "hardhat" || network_ == "localhost") {
-      await deploy('ERC20', {
-        from: deployer,
-        args: [
-          "Yearn Finance",
-          "YFI",
-          "8"
-        ]
-      });
-      const ERC20 = await deployments.get('ERC20');
-      await save("ERC20", ERC20);
-      
-      // mint tokens
-      await execute('ERC20', {
-        from: deployer,
-      },
-        "addMinter",
-        deployer
-      );
-
-      await execute('ERC20', {
-        from: deployer
-      },
-        "mint",
-        deployer,
-        200
-      );
-
-      // update mock yfi address
-      yfi = ERC20.address;
-    }
-    
-
-    await deploy('CYFI', {
-      from: deployer,
-      args:[
-        yfi,
-        Unitroller.address,
-        Model.address,
-        "200000000000000000000000000",
-        "Anchor YFI",
-        "anYFI",
-        "8",
-        deployer,
-        delegateRegistry
-      ]
-    });
-    const CYFI = await deployments.get('CYFI');
-    await save("anYFI", CYFI);
-  };
+  await deploy('CYFI', {
+    from: deployer,
+    args:[
+      yfi,
+      Unitroller.address,
+      Model.address,
+      "200000000000000000000000000",
+      "Anchor YFI",
+      "anYFI",
+      "8",
+      deployer,
+      delegateRegistry
+    ]
+  });
+  const CYFI = await deployments.get('CYFI');
+  await save("anYFI", CYFI);
+};
 
 module.exports.dependencies = ['DolaInterestRateModel', 'Unitroller'];
 module.exports.tags = ['anYFI'];
