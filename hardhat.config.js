c445903cbece19a4ac8e2f4465ae387ef2270229
Masaud <sophismay@gmail.com>
2021-04-25 23:45:03 +0200

    fix: removing hardhat added local config and checks
    hardhat config: removed local network indices from named accounts
    deploy files: removed local network checks from deployment files
    added deployment/* files that were removed from git

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,6 @@ require("@nomiclabs/hardhat-waffle");
 require('@nomiclabs/hardhat-etherscan')
 require('hardhat-deploy')
 require('dotenv').config()
-require('solidity-coverage')
 
 require('@eth-optimism/smock/build/src/plugins/hardhat-storagelayout')
 
@@ -22,9 +21,7 @@ module.exports = {
       }
     }
   },
-  defaultNetwork: "hardhat",
   networks:{
-    hardhat: {},
     rinkeby:{
       url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
       accounts: [process.env.RINKEBY_PRIVKEY]
@@ -40,34 +37,27 @@ module.exports = {
   namedAccounts: {
     dai: {
       1:"0x6b175474e89094c44da98b954eedeac495271d0f",
-      4:"0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735",
-      "hardhat": 4
+      4:"0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735"
     },
     ethFeed:{
       1:"0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
-      4:"0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
-      "hardhat": 5
+      4:"0x8A753747A1Fa494EC906cE90E9f37563A8AF630e"
     },
     yfi: {
-      1:"0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e",
-      "hardhat": 6
+      1:"0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e"
     },
     delegateRegistry: {
-      1:"0x469788fE6E9E9681C6ebF3bF78e7Fd26Fc015446",
-      "hardhat": 7
+      1:"0x469788fE6E9E9681C6ebF3bF78e7Fd26Fc015446"
     },
     sushiFeed:{
-      1:"0xe572CeF69f43c2E488b33924AF04BDacE19079cf",
-      "hardhat": 8
+      1:"0xe572CeF69f43c2E488b33924AF04BDacE19079cf"
     },
     sushiExchangeRate:{
-      1:"0x851a040fC0Dcbb13a272EBC272F2bC2Ce1e11C4d",
-      "hardhat": 9
+      1:"0x851a040fC0Dcbb13a272EBC272F2bC2Ce1e11C4d"
     },
     gov:{
       1:"0x926dF14a23BE491164dCF93f4c468A50ef659D5B",
-      4:0,
-      "hardhat": 1
+      4:0
     },
     inv: {
       1: "0x41d5d79431a913c4ae7d69a668ecdfe5ff9dfb68"
@@ -77,4 +67,3 @@ module.exports = {
     }
   }
 };
-
